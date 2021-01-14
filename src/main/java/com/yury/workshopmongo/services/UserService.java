ea54e65b899f7b41643d5e02439b0fdacece0914
Deyvid Lopes <deyvidyury@yahoo.com.br>
2021-01-13 22:23:55 -0300

    Conectando ao mongodb com repository e service

---
+++ src/main/java/com/yury/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.yury.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yury.workshopmongo.domain.User;
+import com.yury.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+}
