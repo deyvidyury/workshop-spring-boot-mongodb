c0ae1059280a49d05b4a59f0d253cccd3e689bd0
Deyvid Lopes <deyvidyury@yahoo.com.br>
2021-01-15 22:09:05 -0300

    obtendo post por id

---
+++ src/main/java/com/yury/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.yury.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yury.workshopmongo.domain.Post;
+import com.yury.workshopmongo.repository.PostRepository;
+import com.yury.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+	}
+	
+}
